0ec36d7d99bfde6f1879ea772735d480d498ed89
Enzo Vignolo <evignolo@cfotechlatam.com>
2024-09-27 16:30:02 -0300

    feat: adding pipelines validations and handeling unhandled rejections that reach this level

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //add validation pipeline
+  app.useGlobalPipes(new ValidationPipe());
+  //
+  await app.listen(process.env.PORT);
+  console.log(`[OK] API RUNNING ON PORT ${process.env.PORT}`);
+  process.on('unhandledRejection', (error, promise) => {
+    console.log('Unhandled rejection at ', promise, `reason: ${error}`);
+  });
 }
 bootstrap();
